60da8b48069031e0644d6459b19931a581881854
Kanhaiya Sinha <sinhakanha21@gmail.com>
2024-03-11 17:51:40 +0530

    Updated Redux User and Loader store nd Slice

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
+import { Provider } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
       <App />
   </Provider>
-    
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
