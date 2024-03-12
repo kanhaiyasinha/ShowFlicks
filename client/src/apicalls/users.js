f8b493ebfee5afa2d4985c973e432c14f8aa823c
Kanhaiya Sinha <sinhakanha21@gmail.com>
2024-03-12 19:49:42 +0530

    working on protected route

--- client/src/apicalls/users.js
+++ client/src/apicalls/users.js
@@ -11,9 +11,20 @@ export const RegisterUser = async (payload) => {
     }
 }
 
+//Login new users
 export const LoginUser = async (payload) => {
     try {
         const response = await axiosInstance.post('api/users/login', payload)
+        return response.data
+    } catch (error) {
+        return error
+    }
+}
+
+//Get Currrent users
+export const GetCurrentUser = async () => {
+    try {
+        const response = await axiosInstance.get('api/users/currentUser')
         return response.data
     } catch (error) {
         return error
---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,92 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { GetCurrentUser } from '../apicalls/users';
+import {useDispatch} from 'react-redux'
+import {ShowLoading, HideLoading} from '../redux/loadersSlice'
+import {SetUser} from '../redux/loadersSlice'
+import { message } from 'antd';
+
+const ProtectedRoute = () => {
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const getCurrentUser = async () => {
+        try {
+            //show loader
+            dispatch(ShowLoading())
+            const response = await GetCurrentUser()
+            //hide loader
+            dispatch(HideLoading())
+            if (response.success) {
+
+                //dispatch current user to user reducer
+                dispatch(SetUser(response.data))
+
+            } else {
+                //dispatch null to userreducer
+                dispatch(SetUser(null))
+                //show some error message
+                message.error(response.message)
+                //Delete expired token from local storage
+                localStorage.removeItem("token")
+                //redirect to login page
+                navigate('/login')
+            }
+        } catch (error) {
+            //hide loader
+            //dispatch null to userreducer
+            //redirect to login page
+        }
+    }
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            //get current user and set it in user store
+
+        } else {
+            navigate('/login')
+        }
+    }, [])
+
+    return (
+        user &&
+        (
+            <div className="layout p-1">
+                <div className="header bg-primary flex justify-between p-2">
+                    <div>
+                        <h1 className="text-2xl text-white cursor-pointer"
+                            onClick={() => navigate("/")}
+                        >Book My Show</h1>
+                    </div>
+
+                    <div className="bg-white p-1 flex gap-1">
+                        <i className="ri-shield-user-line text-primary mt-1"></i>
+                        <h1
+                            className="text-sm underline"
+                            onClick={() => {
+                                if (user.isAdmin) {
+                                    navigate("/admin");
+                                } else {
+                                    navigate("/profile");
+                                }
+                            }}
+                        >
+                            {user.name}
+                        </h1>
+
+                        <i
+                            className="ri-logout-box-r-line mt-1"
+                            onClick={() => {
+                                localStorage.removeItem("token");
+                                navigate("/login");
+                            }}
+                        ></i>
+                    </div>
+                </div>
+                <div className="content mt-1 p-1">{children}</div>
+            </div>
+        )
+    );
+}
+
+export default ProtectedRoute
