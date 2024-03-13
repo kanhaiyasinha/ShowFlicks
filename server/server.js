64c4aced842509faa1333fc8b72165f71c7d3306
Kanhaiya Sinha <sinhakanha21@gmail.com>
2024-03-14 00:07:47 +0530

    Added Movie api

--- server/server.js
+++ server/server.js
@@ -6,10 +6,12 @@ require('dotenv').config()
 const app = express()
 const dbConfig = require('./config/dbConfig')
 const userRoutes = require('./routes/userRoutes')
+const movieRoutes = require('./routes/movieRoutes')
 
 //we use express.json() to body parsing of json data we're getting from the client
 app.use(express.json())
 app.use('/api/users', userRoutes.router)
+app.use('/api/movies', movieRoutes.router)
 
 
 
