8366832cad0996f5b9e983c17841c522b8cba9a5
Kanhaiya Sinha <sinhakanha21@gmail.com>
2024-04-24 23:46:52 +0530

    Added Movie Page and Get Movie Details

--- client/src/pages/TheatresForMovie/index.js
+++ client/src/pages/TheatresForMovie/index.js
@@ -1,8 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
+import { GetMovieById } from "../../apicalls/movies";
+import { GetTheatresByMovie } from "../../apicalls/theatres";
+import { message } from "antd";
+import moment from "moment";
 
 const TheatresForMovie = () => {
+  const [movie, setMovie] = useState();
+  const [date,setDate] = useState(moment().format("YYYY-MM-DD"))
+  const params = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const getData = async () => {
+    const movieId = params.movieId
+    try {
+      dispatch(ShowLoading());
+      const response = await GetMovieById(movieId);
+      if (response.success) {
+        setMovie(response.data);
+      } else {
+        message.error(response.message);
+      }
+      dispatch(HideLoading());
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  }
+
+  const getTheatres = async () => {
+
+  }
+
+  useEffect(() => {
+    getData();
+    
+  }, [])
+
+  useEffect( () =>{
+    getTheatres();
+  },[])
+
   return (
-    <div>TheatresForMovie</div>
+    <div>
+      {movie && (
+        <div>
+          {/* movie information */}
+          <div className="flex justify-between items-center mb-2">
+            <div>
+              <h1 className="text-2xl uppercase">
+                {movie.title} ({movie.language})
+              </h1>
+              <h1 className="text-md">Duration : {movie.duration} mins</h1>
+              <h1 className="text-md">
+                Release Date : {moment(movie.releaseDate).format("MMM Do yyyy")}
+              </h1>
+              <h1 className="text-md">Genre : {movie.genre}</h1>
+            </div>
+            <div>
+              <h1 className="text-md">Select Date</h1>
+              <input
+                type="date"
+                min={moment().format("YYYY-MM-DD")}
+                value={date}
+                onChange={(e) => {
+                  setDate(e.target.value);
+                  navigate(`/movie/${params.movieId}?date=${e.target.value}`);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
