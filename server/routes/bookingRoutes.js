bf7cf679041cfce5c171c806ec2493fd7db42a2d
Kanhaiya Sinha <sinhakanha21@gmail.com>
2024-05-04 20:16:48 +0530

    Final commit

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -27,7 +27,8 @@ router.post('/make-payment',authMiddleware,async(req,res)=>{
             data:transactionId
         })
 
-    }catch(err){    
+    }catch(err){  
+         
         res.send({
             success:false,
             message:err.message
@@ -54,6 +55,7 @@ router.post("/book-show", authMiddleware, async (req, res) => {
         data: newBooking,
       });
     } catch (error) {
+      
       res.send({
         success: false,
         message: error.message,
@@ -62,38 +64,38 @@ router.post("/book-show", authMiddleware, async (req, res) => {
   });
 
 
-//   router.get("/get-bookings", authMiddleware, async (req, res) => {
-//     try {
-//       const bookings = await Booking.find({ user: req.body.userId })
-//         .populate("user")
-//         .populate("show")
-//             .populate({
-//                 path: "show",
-//                 populate: {
-//                 path: "movie",
-//                 model: "movies",
-//                 },
-//             })
-//             .populate({
-//                 path: "show",
-//                 populate: {
-//                 path: "theatre",
-//                 model: "theatres",
-//                 },
-//             });
+  router.get("/get-bookings", authMiddleware, async (req, res) => {
+    try {
+      const bookings = await Booking.find({ user: req.body.userId })
+        .populate("user")
+        .populate("show")
+            .populate({
+                path: "show",
+                populate: {
+                path: "movie",
+                model: "movies",
+                },
+            })
+            .populate({
+                path: "show",
+                populate: {
+                path: "theatre",
+                model: "theatres",
+                },
+            });
 
-//       res.send({
-//         success: true,
-//         message: "Bookings fetched successfully",
-//         data: bookings,
-//       });
-//     } catch (error) {
-//       res.send({
-//         success: false,
-//         message: error.message,
-//       });
-//     }
-//   });
+      res.send({
+        success: true,
+        message: "Bookings fetched successfully",
+        data: bookings,
+      });
+    } catch (error) {
+      res.send({
+        success: false,
+        message: error.message,
+      });
+    }
+  });
 
 
 exports.router = router
